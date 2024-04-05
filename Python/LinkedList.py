8f4ec8b5bc270a434109e9368c1352c002bb9707
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2024-04-05 18:45:17 +0000

    Remove Node in LinkedList

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -27,14 +27,31 @@ class LinkedList:
         new_node.set_next_node(self.get_head_node())
         self.head_node = new_node
     
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+
+        if(current_node.get_value() == value_to_remove):
+            self.head_node = current_node.get_next_node()
+        else:
+            while(current_node):
+                next_node = current_node.get_next_node()
+                if(next_node.get_value() == value_to_remove):
+                    current_node.set_next_node(next_node.get_next_node())
+                    
+                    #Remove reference to node held in current node
+                    current_node = None
+                else:
+                    current_node = next_node
+
+    
     def printList(self, show_nodes=False):
-        output = ""
+        output = "(H) "
         current_node = self.head_node
         while(current_node):
             if(current_node.get_value() != None):
-                output += str(current_node.get_value()) + "   "
+                output += str(current_node.get_value())
                 if(current_node.get_next_node() != None and show_nodes):
-                    output += "\n|\n∨\n"
+                    output += " -> "
             current_node = current_node.get_next_node()
         return output
 
@@ -43,4 +60,8 @@ list = LinkedList(5)
 list.insert_beginning(20)
 list.insert_beginning(100)
 list.insert_beginning(2000)
+print(list.printList(True))
+
+list.remove_node(100)
+print("Removing Node")
 print(list.printList(True))
