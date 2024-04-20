14fb9cfb31c6933be293926ed5a645710149769c
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2024-04-20 15:47:36 +0000

    Add helper functions for Queue

--- Queue.py
+++ Queue.py
@@ -1,4 +1,5 @@
 class Node:
+  
   def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
@@ -18,3 +19,23 @@ class Queue:
     self.tail = None
     self.max_size = max_size
     self.size = 0
+
+
+  def peek(self):
+    if self.size > 0:
+      return self.head.get_value()
+    else:
+      print("No orders waiting!")
+  
+  def get_size(self):
+    return self.size
+  
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    else:
+      return self.max_size > self.get_size()
+    
+  def is_empty(self):
+    return self.size == 0
+
