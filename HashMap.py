e13456ae5f04fa5d514290f6e420b99765bb1cfa
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2024-04-20 16:00:14 +0000

    Add a .hash method to HashMap

--- HashMap.py
+++ HashMap.py
@@ -2,3 +2,8 @@ class HashMap:
   def __init__(self, array_size):
     self.array_size = array_size
     self.array = [None for item in range(array_size)]
+
+  def hash(self, key, count_collisions=0):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code + count_collisions
